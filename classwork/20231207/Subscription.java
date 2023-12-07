eb2683976119b51f28f836e5ca8ecb4d6e430ee7
Rush Han <raushanhan2808@icloud.com>
2023-12-07 14:44:38 +0300

    Created files User.java and Subscriptions.java, they are not compiled

---
+++ classwork/20231207/Subscription.java
@@ -0,0 +1,19 @@
+public class Subscription {
+	
+	private int user1_id;
+	private int user2_id;
+	
+	public Subscription(int user1_id, int user2_id) {
+		this.user1_id = user1_id;
+		this.user2_id = user2_id;
+	}
+
+	public int getFirstId() {
+		return user1_id;
+	}
+	
+	public int getSecondId() {
+		return user2_id;
+	}
+
+}
